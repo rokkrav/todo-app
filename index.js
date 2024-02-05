36ec7e213da34beb8075601ecb64181fbced7b6e
rokkrav <mailtoravone@gmail.com>
2024-02-05 23:02:49 +0530

    todo app setup done

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post("/create", (req, res) => {});
+
+app.get("/todos", (req, res) => {});
+
+app.put("/completed", (req, res) => {});
+
+app.listen(3000);
